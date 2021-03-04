b320e493d1b524fc08adcb9a37c57f9f5e97ed5c
Ishanax <67413079+Ishanax@users.noreply.github.com>
2021-03-04 17:39:00 +0100

    added nav

--- app.js
+++ app.js
@@ -72,7 +72,17 @@ function isValid() {
    }
  }
 
+//nav bar
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myNav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
 
 
 
